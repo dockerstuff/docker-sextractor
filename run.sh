a8b733cfb80540dd8b59edc3e15c47d55a10934f
peptidoglycanthrope <igriswol@andrew.cmu.edu>
2018-06-12 20:52:43 -0400

    Automatically rename *.cat to *.csv

--- run.sh
+++ run.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+#run the image with a specified volume
 docker run -v /tmp/output:/../work dige-sextractor
 
+#make an output directory based on the date
 folderName=$(date "+output-%Y-%m-%d-%H-%M-%S")
 mkdir run/$folderName
 
+#move output files into that directory
 mv /tmp/output/* run/$folderName
 
+for subdir in $(ls run/$folderName); do
+    for ff in $(ls run/$folderName/$subdir | grep ".cat"); do
+        NAME=`echo "$ff"| cut -d'.' -f1`
+	cp run/$folderName/$subdir/$ff run/$folderName/$subdir/$NAME.csv
+	rm run/$folderName/$subdir/$ff  
+    done
+done
