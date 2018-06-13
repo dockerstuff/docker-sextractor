18693d89255299819928ee03ac3a9e35075d9a49
peptidoglycanthrope <igriswol@andrew.cmu.edu>
2018-06-13 13:41:54 -0400

    Cleaned up folder names

--- Dockerfile
+++ Dockerfile
@@ -5,9 +5,9 @@ VOLUME /work
 ENV SEX_PKG sextractor-2.19.5-1.x86_64.rpm
 
 #add files in run and setup to working directory
-ADD ./run .
+ADD ./dige .
 ADD ./setup .
 
 RUN rpm --install --quiet $SEX_PKG
 
-CMD bash run.sh
+CMD bash dige.sh
--- run.sh
+++ run.sh
@@ -5,15 +5,15 @@ docker run -v /tmp/output:/../work dige-sextractor
 
 #make an output directory based on the date
 folderName=$(date "+output-%Y-%m-%d-%H-%M-%S")
-mkdir run/$folderName
+mkdir dige/$folderName
 
 #move output files into that directory
-mv /tmp/output/* run/$folderName
+mv /tmp/output/* dige/$folderName
 
-for subdir in $(ls run/$folderName); do
-    for ff in $(ls run/$folderName/$subdir | grep ".cat"); do
+for subdir in $(ls dige/$folderName); do
+    for ff in $(ls dige/$folderName/$subdir | grep ".cat"); do
         NAME=`echo "$ff"| cut -d'.' -f1`
-	cp run/$folderName/$subdir/$ff run/$folderName/$subdir/$NAME.csv
-	rm run/$folderName/$subdir/$ff  
+	cp dige/$folderName/$subdir/$ff dige/$folderName/$subdir/$NAME.csv
+	rm dige/$folderName/$subdir/$ff  
     done
 done
